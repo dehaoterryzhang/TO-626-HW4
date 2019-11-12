f4a15c521a22741aaeb053bbb55010ea962e2f58
terryz <terryz@umich.edu>
2019-11-12 09:02:17 -0500

    some change

---
+++ app/src/main/java/com/example/hw4/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hw4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// some change
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
