62e484919a832e0ee7ff683b746c1cfb10816192
terryz <terryz@umich.edu>
2019-11-12 14:29:30 -0500

    some changes made here

---
+++ app/src/main/java/com/example/hw4/Bird.java
@@ -0,0 +1,16 @@
+package com.example.hw4;
+
+public class Bird {
+    public String nameofbird;
+    public String zipcode;
+    public String nameofperson;
+
+    public Bird() {
+    }
+
+    public Bird(String nameofbird, String zipcode, String nameofperson) {
+        this.nameofbird = nameofbird;
+        this.zipcode = zipcode;
+        this.nameofperson = nameofperson;
+    }
+}
--- app/src/main/java/com/example/hw4/MainActivity.java
+++ app/src/main/java/com/example/hw4/MainActivity.java
@@ -3,14 +3,49 @@ package com.example.hw4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-// some changes added
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextNameBird, editTextZipcode, editTextNamePerson;
+    Button buttonSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNameBird = findViewById(R.id.editTextNameBird);
+        editTextZipcode = findViewById(R.id.editTextZipcode);
+        editTextNamePerson = findViewById(R.id.editTextNamePerson);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(this);
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Bird Obs");
+
+
+
+        if (view == buttonSubmit) {
+
+        }
+
+
     }
 }
