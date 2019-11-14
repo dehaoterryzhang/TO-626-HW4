3987c5561169a6501cd189c422a9dd239a354678
terryz <terryz@umich.edu>
2019-11-14 08:38:00 -0500

    some changes made here

--- app/src/main/java/com/example/hw4/MainActivity.java
+++ app/src/main/java/com/example/hw4/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.hw4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -29,21 +35,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonSubmit.setOnClickListener(this);
 
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.itemReport) {
+            Toast.makeText(this, "You are already in report page!", Toast.LENGTH_SHORT).show();
+
+        } else if (item.getItemId() == R.id.itemSearch) {
+            Intent SearchIntent = new Intent(this, SearchActivity.class);
+            startActivity(SearchIntent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onClick(View view) {
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Bird Obs");
-
-
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("BirdObs");
 
         if (view == buttonSubmit) {
 
+            String createNameBird = editTextNameBird.getText().toString();
+            String createZipcode = editTextZipcode.getText().toString();
+            String createNamePerson = editTextNamePerson.getText().toString();
+
+            Bird createBird = new Bird(createNameBird, createZipcode, createNamePerson);
+
+            myRef.push().setValue(createBird);
         }
 
 
---
+++ app/src/main/java/com/example/hw4/SearchActivity.java
@@ -0,0 +1,116 @@
+package com.example.hw4;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editTextTypeZip;
+    Button buttonSearch;
+    TextView textViewPrompt, staticBird, staticPerson, textViewBird, textViewSighter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        editTextTypeZip = findViewById(R.id.editTextTypeZip);
+        buttonSearch = findViewById(R.id.buttonSearch);
+        textViewPrompt = findViewById(R.id.textViewPrompt);
+        textViewBird = findViewById(R.id.textViewBird);
+        textViewSighter = findViewById(R.id.textViewSighter);
+        staticBird = findViewById(R.id.staticBird);
+        staticPerson = findViewById(R.id.staticPerson);
+
+        buttonSearch.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.itemReport) {
+            Intent ReportIntent = new Intent(this, MainActivity.class);
+            startActivity(ReportIntent);
+
+        } else if (item.getItemId() == R.id.itemSearch) {
+            Toast.makeText(this, "You are already in search page!", Toast.LENGTH_SHORT).show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("BirdObs");
+
+        if (view == buttonSearch) {
+
+            String findZip = editTextTypeZip.getText().toString();
+
+            myRef.orderByChild("zipcode").equalTo(findZip).addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    Bird foundBird = dataSnapshot.getValue(Bird.class);
+                    String findBirdName = foundBird.nameofbird;
+                    String findSighter = foundBird.nameofperson;
+
+                    textViewBird.setText(findBirdName);
+                    textViewSighter.setText(findSighter);
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+    }
+}
